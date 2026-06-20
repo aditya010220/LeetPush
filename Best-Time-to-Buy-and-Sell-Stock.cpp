ee4fd0983025d9705a40c0c1aac95f6b21ff3bbf
cursedMan <152655952+aditya010220@users.noreply.github.com>
2026-06-21 04:32:39 +0530

    [121] [Time Beats: 6.26%] [Memory Beats: 24.52%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.cpp
+++ Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -1,15 +1,18 @@
 1class Solution {
 2public:
 3    int maxProfit(vector<int>& prices) {
-4        int n = prices.size();
-5        int mine = INT_MAX;
-6        int maxi = 0;
-7        for(int i =0;i<n;i++){
-8            mine = min(mine , prices[i]);
-9             maxi = max(maxi , prices[i]-mine);
-10
+4       int n = prices.size();
+5       int profit=0;
+6       int maxi = INT_MIN;
+7       int small=INT_MAX;
+8       for(int i =0;i<n;i++){
+9        if(small>prices[i]){
+10            small = prices[i];
 11        }
-12        return maxi;
-13    }
-14};
-15
+12        profit = prices[i]-small;
+13        maxi = max(maxi,profit);
+14       }
+15       return maxi;
+16    }
+17};
+18
