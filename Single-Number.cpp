703dc768d4135477ab0165667e7ee673901fa7d5
cursedMan <152655952+aditya010220@users.noreply.github.com>
2026-07-02 01:58:52 +0530

    [136] [Time Beats: 100.00%] [Memory Beats: 58.39%] - LeetPush

--- Single-Number.cpp
+++ Single-Number.cpp
@@ -1,15 +1,10 @@
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        unordered_map<int,int>mp;
-5        for(int i =0;i<nums.size();i++){
-6            mp[nums[i]]++;
-7        }
-8        for(auto i : mp){
-9            if(i.second==1){
-10                return i.first;
-11            }
-12        }
-13        return 0;
-14    }
-15};
+4        int ans =0;
+5       for(int i =0;i<nums.size();i++){
+6        ans = ans^nums[i];
+7       }
+8       return ans;
+9    }
+10};
