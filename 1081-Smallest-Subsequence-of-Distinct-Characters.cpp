8ae48d0e4f9700e9b548e77046b85e264cb2d89d
cursedMan <152655952+aditya010220@users.noreply.github.com>
2026-07-19 19:52:50 +0530

    [1081] [Time Beats: 100.00%] [Memory Beats: 79.99%] - LeetPush

---
+++ 1081-Smallest-Subsequence-of-Distinct-Characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+
+        vector<int> last(26);
+
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<bool> visited(26, false);
+
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            char ch = s[i];
+
+            if (visited[ch - 'a'])
+                continue;
+
+            while (!ans.empty() &&
+                   ans.back() > ch &&
+                   last[ans.back() - 'a'] > i) {
+
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        return ans;
+    }
+};
