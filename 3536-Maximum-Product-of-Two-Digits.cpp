21c6fcd4043e1465973975a5e16b7ee4b7d15537
cursedMan <152655952+aditya010220@users.noreply.github.com>
2026-07-25 23:43:06 +0530

    [3536] [Time Beats: 23.15%] [Memory Beats: 5.88%] - LeetPush

--- 3536-Maximum-Product-of-Two-Digits.cpp
+++ 3536-Maximum-Product-of-Two-Digits.cpp
@@ -1,18 +1,11 @@
+// someone wrote this , it's good 
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int>digits;
-        while(n>0){
-            digits.push_back(n%10);
-            n=n/10;
-        }
-        int maxi = INT_MIN;
-        for(int i =0;i<digits.size();i++){
-            for(int j = i+1;j<digits.size();j++){
-                int mul = digits[i]*digits[j];
-                maxi = max(maxi,mul);
-            }
-        }
-        return maxi;
+        
+        string s = to_string(n);
+        sort(s.rbegin() , s.rend());
+        int maxProduct = (s[0]-'0')*(s[1]-'0');
+        return maxProduct;
     }
 };
