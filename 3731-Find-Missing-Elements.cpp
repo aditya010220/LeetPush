1257137bf8d755ef4890224e86292258a7cb9e8f
cursedMan <152655952+aditya010220@users.noreply.github.com>
2026-08-04 12:01:10 +0530

    [3731] [Time Beats: 7.46%] [Memory Beats: 8.08%] - LeetPush

--- 3731-Find-Missing-Elements.cpp
+++ 3731-Find-Missing-Elements.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-       int small = *min_element(nums.begin(), nums.end());
-       int large = *max_element(nums.begin(), nums.end());
-        vector<int>ans;
-        unordered_set<int>st;
-        for(int x:nums){
-            st.insert(x);
-        }
-        for(int i = small+1;i<large;i++){
-            if(!(st.count(i))){
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+
+        // this is equivalent to the 
+        //  for(int x:nums){
+        //     st.insert(x);
+        // }
+
+
+        unordered_set<int> st(nums.begin(), nums.end());
+        vector<int> ans;
+
+        for (int i = mn + 1; i < mx; i++) {
+            if (!st.count(i)) {
                 ans.push_back(i);
             }
         }
+
         return ans;
     }
 };
